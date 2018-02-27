30575c01948a4fb831f7446c5b84eec18bcf9dca
Jami <jamischwarzwalder@gmail.com>
2018-02-27 23:46:53 +0000

    building out form with sample file

---
+++ s3/upload.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'app/start.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Upload</title>
+    </head>
+    <body>
+        <form action ="" method="post" enctype="multipart/form-data">
+            <input type="file" name="file"/>
+            <input type="submit" value="Upload"/>
+        </form>
+    </body>
+    
+</html>
