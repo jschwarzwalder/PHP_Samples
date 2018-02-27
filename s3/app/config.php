e868944e7b70e978a40f2c29b0f00a5878efceba
Jami <jamischwarzwalder@gmail.com>
2018-02-27 23:34:21 +0000

    building dummy files for uploading to s3

---
+++ s3/app/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    's3' => [
+        'key'=> 'AKIAJEISYSAMPLE',
+        'secret' => '2wDSbEoHi6PUHQm6hw3SECRET',
+        'bucket' => 's3.website.com'
+        ]
+    ];
+    
+    # details can be found in this video https://www.youtube.com/watch?v=HDxCDdZFh9g
---
+++ s3/app/start.php
@@ -0,0 +1,14 @@
+<?php
+
+use Aws\S3\S3Client;
+
+require 'vendor/autoload.php';
+
+$config = require('config.php');
+
+// S3 
+$s3 = S3Client:: factory([
+        'key' => $config['s3']['key'],
+        'secret' => $config['s3']['secret']
+    
+    ])
