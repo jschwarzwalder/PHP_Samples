d63fc5258e444cd54c2b5f861ca0004f8041f5b3
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-03-24 22:06:02 -0700

    processing csv file

--- s3/csvProcess.php
+++ s3/csvProcess.php
@@ -26,8 +26,89 @@ $config['s3']['bucket'] = $new_bucket;
 echo "<p>Reading file</p>";
 
 $myfile = fopen($temp_name, "r") or die("<p>Unable to open file!</p>");
-echo fread($myfile,filesize($temp_name));
+$PHP1 = array('total_customers' => 0, 'total_requests' => 0 ); //PHP SDK V1
+$PHP2 = array('total_customers' => 0, 'total_requests' => 0 ); //PHP SDK V2
+$PHP3 = array('total_customers' => 0, 'total_requests' => 0 ); //PHP SDK V3
+$PHPSDK = array('total_customers' => 0, 'total_requests' => 0 ); //PHP SDK Total
+$SDK = array('total_customers' => 0, 'total_requests' => 0 ); //All SDKs
+//echo fread($myfile,filesize($temp_name));
+
+// $data = fgetcsv($myfile, 0, ",");
+//var_dump( $data);
+//array(5) { 
+//	[0]=> string(4) "tool" 
+//	[1]=> string(7) "service" 
+//	[2]=> string(9) "customers" 
+//	[3]=> string(8) "requests" 
+//	[4]=> string(0) "" }
+
+while (($data = fgetcsv($myfile, 0, ",")) !== FALSE) { 
+	$tool = $data[0];
+	//echo "<p>" . $tool . "</p>";
+	if ($tool == "PHP SDK V1"){
+		$service['name'] = $data[1];	
+		$service['customers'] = intval($data[2]);
+		$service['requests'] = intval($data[3]);
+		array_push($PHP1, $service);
+		$PHP1['total_customers'] += $service['customers'];
+		$PHP1['total_requests'] += $service['requests'];
+		//var_dump($PHP1);
+	} else if ($tool == "PHP SDK V2") {
+		$service['name'] = $data[1];	
+		$service['customers'] = intval($data[2]);
+		$service['requests'] = intval($data[3]);
+		array_push($PHP2, $service);
+		$PHP2['total_customers'] += $service['customers'];
+		$PHP2['total_requests'] += $service['requests'];
+	} else if ($tool == "PHP SDK V3") {
+		$service['name'] = $data[1];	
+		$service['customers'] = intval($data[2]);
+		$service['requests'] = intval($data[3]);
+		array_push($PHP3, $service);
+		$PHP3['total_customers'] += $service['customers'];
+		$PHP3['total_requests'] += $service['requests'];
+	} else if ($tool == "PHP SDK Total") {
+		$service['name'] = $data[1];	
+		$service['customers'] = intval($data[2]);
+		$service['requests'] = intval($data[3]);
+		array_push($PHPSDK, $service);
+		$PHPSDK['total_customers'] += $service['customers'];
+		$PHPSDK['total_requests'] += $service['requests'];
+	} else if ($tool == "All SDKs") {
+		add_to_array($SDK, $data);
+		
+	}
+}
+
 fclose($myfile);
+
+function cal_percents(&$sdk) {
+	$total_customers = floatval($sdk['total_customers']);
+	$total_requests = $sdk['total_requests'];
+	foreach ($sdk as &$service){
+		$service['customer_percent'] = floatval($service['customers'] / $total_customers);
+		$service['requests_percent'] = $service['requests'] / $total_requests;
+	}
+	
+}
+
+function add_to_array(&$sdk, $data){
+		$service['name'] = $data[1];	
+		$service['customers'] = intval($data[2]);
+		$service['requests'] = intval($data[3]);
+		array_push($sdk, $service);
+		$sdk['total_customers'] += $service['customers'];
+		$sdk['total_requests'] += $service['requests'];
+}
+
+
+cal_percents($SDK);
+
+//var_dump($PHP1);
+//var_dump($PHP2);
+//var_dump($PHP3);
+//var_dump($PHPSDK);
+var_dump($SDK);
 
 
 ?>
